98c565e70a20be9cd95370c5db4723acfc16e4b8
bambuco2 <domen.dobnikar@gmail.com>
2022-05-24 12:37:55 +0200

    adding swagger

--- AssignmentIndigoLabs/Program.cs
+++ AssignmentIndigoLabs/Program.cs
@@ -5,13 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
